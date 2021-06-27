2401b62ff33b9d0c0314f5b9f264f1e057d6bee8
juansealvarez <juansealvarez5@gmail.com>
2021-06-27 11:49:03 -0500

    Feat: Carga inicial

---
+++ apuntes.md
@@ -0,0 +1,1 @@
+# Aqui se escriben los apuntes
---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express') //importar express
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const path = require('path')//trabajar con el views en otra ruta
+const app =express(); //objeto app ejecutando express
+
+app.use(express.static(path.join(__dirname,'assets'))); //configurar archivos estaticos
+
+app.set('view engine', 'ejs'); //configurar ejs template
+app.set('views', path.join(__dirname,'/views')); //ruta para directorios de views
+
+app.use(bodyParser.json()); //configurar bodyparser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(session({
+    secret: "123456789",
+    resave: false,
+    saveUninitialized: false
+})); //para trabajar con formularios
+
+app.get('/',(req, res)=>{
+    //Valido si hay un usario en la sesion
+    if (req.session.usuario != null){
+        res.render('index',{
+            usuario:req.session.usuario
+        });
+    }else{
+        res.render("index",{
+            usuario:{
+                nombre: "",
+                pais: "",
+                mail: "",
+                mensaje: ""
+            }
+        });
+    };
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor iniciandose en el puerto ${PORT}`);
+});
